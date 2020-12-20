5635c0fae7cb56ceeb61c9d804c0d8acfa2b13c9
Ojaswa Rajput <69306443+OjaswaRajput@users.noreply.github.com>
2020-12-20 17:24:11 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,12 +4,15 @@ var PLAY = 1;
 var END = 0;
 var gameState=PLAY;
 var Door,DoorImage;
-var DoorGroup,BlockGroup;
+var left,right,lefrImage,RightImage;
+var DoorGroup;
 
 function preload(){
   GhostImage = loadImage("ghost-standing.png");
   TowerImage = loadImage("tower.png");
   DoorImage = loadImage("door.png");
+  leftImage = loadImage("left arrow.png");
+  RightImage = loadImage("right arrow.jpg");
 }
 
 
@@ -20,8 +23,14 @@ createCanvas(600,600);
   Tower.addImage(TowerImage);
   Ghost = createSprite(300,500);
   Ghost.addImage(GhostImage);
+  left = createSprite(200,200);
+  left.addImage(leftImage);
+  right = createSprite(400,200);
+  right.addImage(RightImage);
   Ghost.scale = 0.3
   Tower.scale = 0.9
+  right.scale = 0.4
+  left.scale = 0.1
   Tower.velocityY = 3;
   DoorGroup = new Group();
 }
@@ -41,6 +50,13 @@ background("black");
   if (Tower.y>600){
     Tower.y = 400;
   }
+    if (mousePressedOver(right)) {
+    ( Ghost.x = Ghost.x +5);
+    }
+    
+    if (mousePressedOver(left)) {
+    ( Ghost.x = Ghost.x -5);
+    }
   if(DoorGroup.isTouching(Ghost)||Ghost.y>600){
     gameState = END;
   }
